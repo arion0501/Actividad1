a4b6a077bf0600e56c3fe5aa1263d4af64f25d91
arion0501 <targvi@gmail.com>
2023-10-02 19:55:17 +0200

    3er Commit, ruta inicial actualizada e implementación de 2 inputTexts

--- lib/Actividad1.dart
+++ lib/Actividad1.dart
@@ -1,3 +1,4 @@
+import 'package:actividad1/Registro&Login/vistaLogin.dart';
 import 'package:flutter/material.dart';
 
 class Actividad1 extends StatelessWidget {
@@ -6,9 +7,10 @@ class Actividad1 extends StatelessWidget {
   Widget build(BuildContext context) {
     MaterialApp materialApp = MaterialApp(title: "Actividad 1",
       routes: {
+        '/vistalogin':(context) => vistaLogin(),
 
       },
-      initialRoute: '',
+      initialRoute: '/vistalogin',
     );
 
     return materialApp;
--- lib/Registro&Login/vistaLogin.dart
+++ lib/Registro&Login/vistaLogin.dart
@@ -9,12 +9,43 @@ class vistaLogin extends StatelessWidget{
     _context = context;
 
     Column columna = Column(children: [
+      // Separación Tabla
+      Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+      Text('ACTIVIDAD 1', style: TextStyle(fontSize: 18)),
+
+      Padding(padding: EdgeInsets.symmetric(horizontal: Checkbox.width, vertical: 14), child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Input User ACTIVIDAD1',
+        ),
+      ),
+      ),
+
+      Padding(padding: EdgeInsets.symmetric(horizontal: Checkbox.width, vertical: 14), child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Input Password ACTIVIDAD1',
+        ),
+      ),
+      ),
 
     ],);
 
-    AppBar appBar = AppBar();
+    AppBar appBar = AppBar(
+      title: Text('LOGIN'),
+      backgroundColor: Colors.amber,
+      foregroundColor: Colors.deepOrange,
+    );
 
-    Scaffold scaf = Scaffold();
+    Scaffold scaf = Scaffold(
+      body: columna,
+      backgroundColor: Colors.lightGreen,
+      appBar: appBar,
+    );
 
     return scaf;
   }
