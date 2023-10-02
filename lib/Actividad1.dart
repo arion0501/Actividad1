9e64f00b93ee584c132e87a6823a3561eaeb4718
arion0501 <targvi@gmail.com>
2023-10-02 18:38:22 +0200

    2º Commit con implementación del main y de la vista Login

--- lib/Actividad1.dart
+++ lib/Actividad1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Actividad1 extends StatelessWidget{
+class Actividad1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/Registro&Login/vistaLogin.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class vistaLogin extends StatelessWidget{
+
+  late BuildContext _context;
+
+  @override
+  Widget build(BuildContext context) {
+    _context = context;
+
+    Column columna = Column(children: [
+
+    ],);
+
+    AppBar appBar = AppBar();
+
+    Scaffold scaf = Scaffold();
+
+    return scaf;
+  }
+
+
+
+}
