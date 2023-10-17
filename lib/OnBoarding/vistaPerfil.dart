8e56f8d6a1c1a53ba6aa0387a79c620b3640e8eb
arion0501 <targvi@gmail.com>
2023-10-17 18:36:06 +0200

    22º Commit, implementación de código principal a vistaPerfil, creación del paquete objetosFirestore y dentro de este implementada la clase que guardará los usuarios

---
+++ lib/OnBoarding/vistaPerfil.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class vistaPerfil extends StatelessWidget {
+
+  late BuildContext _context;
+
+  @override
+  Widget build(BuildContext context) {
+
+    _context = context;
+
+    throw UnimplementedError();
+  }
+
+
+
+}
