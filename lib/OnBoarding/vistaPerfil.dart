14a5acc55fe2c93ee26742b42fb8ffea83f7a00b
arion0501 <targvi@gmail.com>
2023-10-17 19:21:28 +0200

    24º Commit, implementada la base de datos en firebase y añadida la función de guardar el perfil del usuario en ésta con el boton aceptar

--- lib/OnBoarding/vistaPerfil.dart
+++ lib/OnBoarding/vistaPerfil.dart
@@ -1,8 +1,33 @@
+import 'package:actividad1/ObjetosFirestore/UsuariosFS.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class vistaPerfil extends StatelessWidget {
 
   late BuildContext _context;
+
+  TextEditingController tecNombre = TextEditingController();
+  TextEditingController tecEdad = TextEditingController();
+  TextEditingController tecPeso = TextEditingController();
+  TextEditingController tecColorOjos = TextEditingController();
+
+  FirebaseFirestore fb = FirebaseFirestore.instance;
+
+  void onClickCancelar() {
+
+  }
+
+  void onClickAceptar() async {
+    UsuariosFS usuario = new UsuariosFS(nombre: tecNombre.text,
+        edad: int.parse(tecEdad.text), peso: double.parse(tecPeso.text),
+        colorOjos: tecColorOjos.text);
+    
+    String uidUsuario = FirebaseAuth.instance.currentUser!.uid;
+    await fb.collection("Usuarios").doc(uidUsuario).set(usuario.toFirestore());
+
+    Navigator.of(_context).popAndPushNamed('/vistaHome');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +36,6 @@ class vistaPerfil extends StatelessWidget {
 
     throw UnimplementedError();
   }
-
 
 
 }
