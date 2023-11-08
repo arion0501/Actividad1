38011bc85e6d2e54d2157669981634523a029cb8
arion0501 <targvi@gmail.com>
2023-11-08 19:06:33 +0100

    48º Commit, modificación de vistaLista para añadir el drawer correctamente con el uso de un InkWell, además del uso de una función.

--- lib/Custom/VistaLista.dart
+++ lib/Custom/VistaLista.dart
@@ -3,26 +3,37 @@ import 'package:flutter/material.dart';
 class vistaLista extends StatelessWidget{
 
   final String sTexto;
-  final int iCodigoColor;
+  final MaterialColor iCodigoColor;
   final double dTamanyoFuente;
+  final int iPosicion;
+  final Function(int indice)? onItemListClickedFun;
+
 
   const vistaLista ({
     required this.sTexto,
     required this.iCodigoColor,
-    required this.dTamanyoFuente});
+    required this.dTamanyoFuente,
+    required this.iPosicion,
+    required this.onItemListClickedFun});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.amber[iCodigoColor],
-        child: Row(
-          children: [
-            Image.asset('Recursos/aiGenerated.png',
-                width: 30,
-                height: 45),
-            Text(sTexto)
-          ],
-        )
+    return InkWell(
+      child: Container(
+          color: iCodigoColor,
+          child: Row(
+            children: [
+              Image.asset('Recursos/aiGenerated.png',
+                  width: 55,
+                  height: 55),
+              Text(sTexto,style: TextStyle(fontSize: dTamanyoFuente)),
+              TextButton(onPressed: null, child: Text(" +",style: TextStyle(fontSize: dTamanyoFuente)))
+            ],
+          )
+      ),
+      onTap: () {
+        onItemListClickedFun!(iPosicion);
+      },
     );
   }
 }
