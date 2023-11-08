7a6158d7ef2173b661e2f52906cc2cc5c4b655c5
arion0501 <targvi@gmail.com>
2023-11-08 18:35:17 +0100

    43º Commit, creación de un drawer personalizado

---
+++ lib/Custom/DrawerCustom.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DrawerCustom extends StatelessWidget {
+
+  Function (int indice)? onItemTap;
+
+  DrawerCustom({Key? key,
+    required this.onItemTap
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Menú Inicio'),
+
+          ),
+          ListTile(
+            title: const Text('1'),
+            leading: Image.asset('resources/aiGenerated.png',
+                width: 45,
+                height: 70),
+            onTap: () {
+              onItemTap!(0);
+            },
+          ),
+          ListTile(
+            title: const Text('2'),
+            leading: Icon(Icons.accessible_forward_rounded, color: Colors.red),
+            onTap: () {
+              onItemTap!(1);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
